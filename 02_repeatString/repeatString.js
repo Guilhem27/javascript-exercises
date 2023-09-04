1a6b46c72d99d3527649ca137662baa7d6ef9419
Guilhem Barroyer <guilhem.barroyer@outlook.com>
2023-09-04 10:59:20 +0200

    Add finished exercices 1 and 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,10 @@
-const repeatString = function() {
-
-};
+const repeatString = function(str, times) {
+    let repeat="";
+    for(let i=0; i<times; i++) {
+        repeat=repeat+str;
+    }
+    return repeat;
+}
 
 // Do not edit below this line
 module.exports = repeatString;
