c683fb10ae88c638e31e5ed9d50cf9418644a8b0
Guilhem Barroyer <guilhem.barroyer@outlook.com>
2023-09-04 13:02:59 +0200

    Add exos 3 and 4

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    const arrayStr=[];
+    for(let i=0;i<(str.length);i++){
+        arrayStr.unshift(str.slice(i,1+i));
+    }
+    return arrayStr.join('');
 };
 
 // Do not edit below this line
--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,13 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, value) {
+    const newArray=[]
+    for(let i=0;i<array.length;i++) {
+        if (array[i]!=value) {
+            newArray.push(array[i]);
+        }
+    }
+    return newArray;
 };
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
