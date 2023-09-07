2f6cce0e1a40156dfececb9ce5e5fcc6b5afcb51
Guilhem Barroyer <guilhem.barroyer@outlook.com>
2023-09-07 09:06:05 +0200

    Add exos 5,6 et 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,9 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(tempF) {
+  return tempF-32
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(tempC) {
+  return tempC+32
 };
 
 // Do not edit below this line
